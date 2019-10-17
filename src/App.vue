c7d0a6f70f047c382b04ef038930dbfc2cd448f5
yntik <eifaurya@gmail.com>
2019-10-17 16:52:22 +0300

    checkbox form

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <template>
-    <FormWithValidation>
-    </FormWithValidation>
+    <checkbox-form>
+
+    </checkbox-form>
 </template>
 
 <script>
     import HelloWorld from './components/HelloWorld.vue'
     import Form from './components/Form.vue'
     import FormWithValidation from './components/FormWithValidation.vue'
+    import CheckboxForm from './components/CheckboxForm.vue'
 
     export default {
         name: 'app',
         components: {
             HelloWorld,
             Form,
-            FormWithValidation
+            FormWithValidation,
+            CheckboxForm
         }
     }
 </script>
